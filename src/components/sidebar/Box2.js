abb6ee0d78ae12004eb286dadcfe5da338bd7988
Serhii Stepaniuk <serhii.stepaniuk@outlook.com>
2022-01-06 09:08:04 +0200

    update sidebar Boxes (make dynamic content loading)

--- src/components/sidebar/Box2.js
+++ src/components/sidebar/Box2.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { sidebarBox2 } from '../../content/sidebarContent';
+
 
 export const Box2 = () => {
+    const { boxTitle, textLinks } = sidebarBox2;
     return (
         <div className="sbox2">
-                    <h5>Integer gravida</h5>
+                    <h5>{boxTitle}</h5>
                     
                     <ul className="style2">
-                        <li>
-                            <a href="#">Semper quis egetmi dolore</a>
-                        </li>
-                        <li>
-                            <a href="#">Quam turpis feugiat dolor</a>
-                        </li>
-                        <li>
-                            <a href="#">Consequat lorem phasellus</a>
-                        </li>
-                        <li>
-                            <a href="#">Amet ornare hendrerit lectus</a>
-                        </li>
+                        {textLinks.map((itemLink, index) => {
+                            return <li key={index} >
+                                <Link to="/contact"> {itemLink}</Link>
+                            </li>;
+                        })}
                     </ul>
                 </div>
     );
--- src/content/sidebarContent.js
+++ src/content/sidebarContent.js
@@ -14,3 +14,24 @@ export const sidebarStyle1TextContent = [
         textLink: 'In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.'
     },
 ];
+
+
+export const sidebarBox1 = {
+    boxTitle: 'Etiam rhoncus',
+    textLinks: [
+        'Semper quis egetmi dolore',
+        'Quam turpis feugiat dolor',
+        'Amet ornare hendrerit lectus',
+        'Quam turpis feugiat dolor',
+    ]
+};
+
+export const sidebarBox2 = {
+    boxTitle: 'Integer gravida',
+    textLinks: [
+        'Semper quis egetmi dolore',
+        'Quam turpis feugiat dolor',
+        'Consequat lorem phasellus',
+        'Amet ornare hendrerit lectus',
+    ]
+};
